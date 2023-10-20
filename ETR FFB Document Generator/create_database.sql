fc9d7ef186c74e2bc0746f09f3007788fa8c261c
John <jfberry67@gmail.com>
2023-10-20 08:25:43 -0400

    db sql updates

--- ETR FFB Document Generator/create_database.sql
+++ ETR FFB Document Generator/create_database.sql
@@ -16,12 +16,14 @@ CREATE TABLE roster (
 
 CREATE TABLE Level1 (
     Infraction VARCHAR(255),
-    Code VARCHAR(50)
+    Code VARCHAR(50),
+    Definition VARCHAR(255)
 );
 
 CREATE TABLE Level2 (
     Infraction VARCHAR(255),
-    Code VARCHAR(50)
+    Code VARCHAR(50),
+    Definition VARCHAR(255)
 );
 
 CREATE TABLE contacts (
@@ -34,26 +36,26 @@ CREATE TABLE contacts (
 
 DELETE FROM Level1;
 
-INSERT INTO Level1 (infraction, code) VALUES
-('Possession of a weapon on center or under center supervision', '5.1a'),
-('Assault', '5.1a'),
-('Threat of assault', '5.1a'),
-('Threat to safety', '5.1a'),
-('Sexual assault', '5.1a'),
-('Drugs: Possession or distribution of drugs on center or under center supervision', '5.2b'),
-('Drugs: Use of drugs as evidenced by a positive drug test', '5.2a'),
-('Alcohol: Possession, consumption, or distribution while on center or under center supervision', '5.3c'),
-('Abuse of Alcohol', '5.3c'),
-('Arrest for a felony or violent misdemeanor on or off center', '5.1a'),
-('Illegal Activity', '5.1a'),
-('Robbery or Extortion', '5.1a'),
-('Arson', '5.1a'),
-('Cruelty to animals', '5.1a'),
-('Inciting a disturbance or creating disorder', '5.1a');
+INSERT INTO Level1 (infraction, code, Definition) VALUES
+('Possession of a weapon on center or under center supervision', '5.1a','Possessing a weapon, as defined below, while on center or while off center but on a center-supervised activity.'),
+('Assault', '5.1a', 'Taking a physical action with the intent to cause immediate bodily harm to another person unless taken in immediate response to another person taking such an action with the intent to prevent its continuation.'),
+('Threat of assault', '5.1a', 'Making a verbal or written threat to harm another person or group. This includes threats made on social media.'),
+('Threat to safety', '5.1a','Any action or threat of action that could jeopardize the safety or well-being of others.'),
+('Sexual assault', '5.1a','Any unwanted or coerced sexual act or behavior, including rape.'),
+('Drugs: Possession or distribution of drugs on center or under center supervision', '5.2b','Possessing or distributing illegal drugs or controlled substances while on center or while off center but on a center-supervised activity. NOTE: Students who refuse to submit to a drug test or provide a sample for drug testing shall be presumed guilty of this infraction.'),
+('Drugs: Use of drugs as evidenced by a positive drug test', '5.2a','Consuming illegal drugs or controlled substances as evidenced by a positive result from a urinalysis test.'),
+('Alcohol: Possession, consumption, or distribution while on center or under center supervision', '5.3c','While on center or while off center but on a center- supervised activity, knowingly: Possessing alcohol, Consuming alcohol, Distributing alcohol to others.'),
+('Abuse of Alcohol', '5.3c','A pattern of alcohol consumption-related incidents demonstrated by receiving more than two Level II “Intoxication on center or under center supervision” infractions where the intoxication is the result of alcohol while enrolled in the program. The 3rd infraction elevates the behavior to Level I Abuse of Alcohol.'),
+('Arrest for a felony or violent misdemeanor on or off center', '5.1a', 'Being arrested by law enforcement for a felony. Being arrested by law enforcement for a misdemeanor involving the use, attempted use, or threatened use of physical force against the person or property of another.'),
+('Illegal Activity', '5.1a', 'Being convicted of a felony or misdemeanor as defined by Federal or state law, where the crime occurred while the student was enrolled in Job Corps.'),
+('Robbery or Extortion', '5.1a','Taking money or possessions of another from his/her person by force or intimidation.'),
+('Arson', '5.1a','The malicious setting of fire to a structure or personal property belonging to another person or entity.'),
+('Cruelty to animals', '5.1a','The torture, ill-treatment, abandonment, willful infliction of injury or pain, beating, maiming, mutilating, or killing of any animal, whether belonging to the individual or another.'),
+('Inciting a disturbance or creating disorder', '5.1a','Persuading, encouraging, instigating, taunting, pressuring or threatening persons to disrupt a peaceful situation. Causing disorder or disrupting a peaceful situation.');;
 
 DELETE FROM Level2;
 
-INSERT INTO Level2 (infraction, code) VALUES
+INSERT INTO Level2 (infraction, code, Definition) VALUES
 ('Possession of a potentially dangerous item', '5.1b'),
 ('Theft/stealing', '5.1b'),
 ('Intoxication on center or under center supervision', '5.3b'),
